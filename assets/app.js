ab716d547177b83fac9be891e149742a112ffa6d
cleave13 <94774869+cleave13@users.noreply.github.com>
2022-01-24 21:28:26 -0700

    declared variables for DOM elements

--- assets/app.js
+++ assets/app.js
@@ -1,1 +1,24 @@
+// The modal should contain a form asking users to fill in the following data:
+// The name of the project
+// The type of project (use a <select> drop-down)
+// The hourly wage for the project
+// The due date for the project (use jQuery UI's datepicker with a minimum date setting in place)
+
+//variables
+const bootstrapCardEl = $("#bootstrap-card");
+const projectFormEl = $("#project-form");
+const projectNameEl = $("#project-name");
+const projectTypeEl = $("#project-type");
+const projectWageEl = $("#project-wage");
+const projectDueDateEl = $("#project-date");
+const tableBodyEl = $("#table-body");
+
+//functions
+
+
+//event listeners
+
+
+//logic
+
 console.log ("hi");
